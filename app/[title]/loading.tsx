b77aeca8dbd27d27a01181993e6d36779fcff7a0
Alexis Gousseau <alexis.gousseau1@gmail.com>
2026-01-07 23:50:48 +0100

    feat: :sparkles: add loading page when load project data

---
+++ app/[title]/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <p className="text-lg text-muted-foreground animate-pulse">
+        Loading project…
+      </p>
+    </div>
+  );
+}
